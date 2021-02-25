2d32648ed0648537f44cdb5dacbcd2013af9849f
yuchun <xx830921xx@gmail.com>
2021-02-25 18:19:50 +0800

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,16 @@
+import random  # 生成隨機數
+
+
 def major():
     print("Keep it logically awesome.")
 
-    #f = open("quotes.txt")
-    #quotes = f.readlines()
-    # f.close()
+    f = open("quotes.txt")  # 讀檔案
+    quotes = f.readlines()
+    f.close()
 
-    # print(quotes)
+    last = len(quotes-1)  # 自動更新最後一列資料的index，這樣可以在文件中新增/刪除行數
+    ran = random.randint(0, last)
+    print(quotes[ran])
 
 
 if __name__ == "__main__":
